11cc5f546be3c5e534d1188b53cfcfd5226793f6
namjiyun <namjiyun3275@gmail.com>
2022-08-25 13:28:05 +0900

    220825

---
+++ Programmers/Level2/전화번호목록.py
@@ -0,0 +1,10 @@
+# 문자열 배열
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1) : 
+        if(phone_book[i]<phone_book[i+1]):
+            if(phone_book[i]==phone_book[i+1][:len(phone_book[i])]):
+                return False
+    return answer

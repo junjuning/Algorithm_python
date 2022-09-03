dcf0c95038db81bf48ca7f7e50061f2dc4d87ac2
namjiyun <namjiyun3275@gmail.com>
2022-09-03 13:26:59 +0900

    220903

---
+++ Programmers/Level2/다리를지나는트럭.py
@@ -0,0 +1,14 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = [0 for i in range(bridge_length)]
+    print(bridge)
+    return answer
+
+
+solution(2, 10, [7, 4, 5, 6])
+
+
+
+
+7, 0
+0, 7

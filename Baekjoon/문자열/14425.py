3a0f2c319d0815308404bb23c52095e2c95764ab
namjiyun <namjiyun3275@gmail.com>
2022-10-13 15:33:30 +0900

    221013

---
+++ Baekjoon/문자열/14425.py
@@ -0,0 +1,13 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+
+
+s = list(sys.stdin.readline().strip() for _ in range(n))
+arr = list(sys.stdin.readline().strip() for _ in range(m))
+
+cnt = 0
+for i in arr:
+    if(i in s):
+        cnt += 1
+        
+print(cnt)

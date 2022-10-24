5d8dbb86e6ad330654ba7438bcc1136df25a1ab9
namjiyun <namjiyun3275@gmail.com>
2022-10-24 22:18:06 +0900

    221024

---
+++ Baekjoon/Bruth Force/1436.py
@@ -0,0 +1,24 @@
+## 실버5
+# 백트래킹 하려고 했는데 안되네
+
+n = int(input())
+i = 0
+cnt = 0
+while(cnt != n):
+    i += 1
+    if('666' in str(i)):
+        cnt += 1
+    
+print(i)
+
+
+# cnt = 0
+# i = 5
+# while(cnt != n) :
+#     i+=1
+#     if('6' in str(i)):
+#         cnt+=1
+
+    
+# print(str(i).replace('6', '666'))
+

908dcef7ab4d51c147ea75f519887a9f6fefb8ac
namjiyun <namjiyun3275@gmail.com>
2022-11-05 11:40:02 +0900

    [BOJ/15651]

--- Baekjoon/백트래킹/15649.py
+++ Baekjoon/백트래킹/15649.py
@@ -9,11 +9,11 @@ arr = []
 def dfs():
     if(len(arr) == m):
         print(' '.join(map(str, arr)))
-    
-    for i in range(1, n+1):
-        if(i not in arr):
-            arr.append(i)
-            dfs()
-            arr.pop()
+    else:
+        for i in range(1, n+1):
+            if(i not in arr):
+                arr.append(i)
+                dfs()
+                arr.pop()
     
 dfs()

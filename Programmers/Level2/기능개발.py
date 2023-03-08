7fb28a8da5322613d1b3c44061f9b7f4616deb68
namjiyun <namjiyun3275@gmail.com>
2023-03-08 16:29:01 +0900

    [PRG/기능개발]

--- Programmers/Level2/기능개발.py
+++ Programmers/Level2/기능개발.py
@@ -2,23 +2,47 @@
 # 데큐 최고
 # 리스트[-1]은 마지막을 가리킴
 
-from collections import deque
+# from collections import deque
 
-def solution(progresses, speeds):
-    q = deque()
-    answer = []
-    for i in range(len(progresses)):
-        day = (100 - progresses[i]) // speeds[i]
-        if((100 - progresses[i]) % speeds[i] != 0):
-            day+=1
-        q.append(day)
+# def solution(progresses, speeds):
+#     q = deque()
+#     answer = []
+#     for i in range(len(progresses)):
+#         day = (100 - progresses[i]) // speeds[i]
+#         if((100 - progresses[i]) % speeds[i] != 0):
+#             day+=1
+#         q.append(day)
     
-    num = 0
-    for i in q:
-        if(i > num):
-            answer.append(1)
-            num = i
+#     num = 0
+#     for i in q:
+#         if(i > num):
+#             answer.append(1)
+#             num = i
             
+#         else:
+#             answer[-1] += 1
+#     return answer
+
+import math
+def solution(progresses, speeds):
+    answer = []
+    num = 0  
+    before = -1
+    now = 0
+    
+    for i in range(len(progresses)):
+        now = math.ceil((100 - progresses[i]) / speeds[i])
+            
+        if i == 0:
+            before = now
+            num = 0
+            
+        if before >= now:
+            num += 1
         else:
-            answer[-1] += 1
+            answer.append(num)
+            before = now
+            num = 1
+
+    answer.append(num)
     return answer

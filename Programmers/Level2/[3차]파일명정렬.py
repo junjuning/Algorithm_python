2e6cf6d0ce58a66253274434c5a11f9869ad9e2f
namjiyun <namjiyun3275@gmail.com>
2023-03-19 16:13:37 +0900

    [PRG/[3차] 파일명 정렬]

---
+++ Programmers/Level2/[3차]파일명정렬.py
@@ -0,0 +1,11 @@
+import re
+
+def solution(files):
+    answer = []
+    arr = []
+    
+    for file in files:
+        arr.append(re.split(r"([0-9]+)", file))
+    sort = sorted(arr, key = lambda x: (x[0].lower(), int(x[1])))
+        
+    return [''.join(s) for s in sort]

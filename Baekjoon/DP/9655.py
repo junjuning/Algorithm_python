fedb7f2357ba6f999dd20ed2ed579322aeea9b03
namjiyun <namjiyun3275@gmail.com>
2023-04-02 15:56:55 +0900

    [BOJ/돌 게임]

---
+++ Baekjoon/DP/9655.py
@@ -0,0 +1,7 @@
+
+n = int(input())
+
+if n % 2 == 0:
+    print("CY")
+else:
+    print("SK")
--- Programmers/Level2/가장큰수.py
+++ Programmers/Level2/가장큰수.py
@@ -1,13 +1,13 @@
 # 구글링함
 # 람다함수,,써야할 줄은 상상도 못했지
 
-def solution(numbers):
-    answer = ''
-    numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x:x*3, reverse=True)
-    return str(int(''.join(numbers)))
+# def solution(numbers):
+#     answer = ''
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key = lambda x:x*3, reverse=True)
+#     return str(int(''.join(numbers)))
 
-solution([6, 10, 2])
+# solution([6, 10, 2])
 
 def solution(numbers):
     answer = []

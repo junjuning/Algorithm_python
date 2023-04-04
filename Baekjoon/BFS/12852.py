a8a19d9344307443b363a3452e4267e15b6c5057
namjiyun <namjiyun3275@gmail.com>
2023-04-04 18:50:02 +0900

    [BOJ/1로 만들기 2]

--- Baekjoon/BFS/12852.py
+++ Baekjoon/BFS/12852.py
@@ -3,8 +3,32 @@ n = int(input())
 q = deque()
 arr = [i for i in range(n + 1)]
 arr[1] = 0
-path  = [i for i in range(n + 1)]
-path[1] = 0
+path  = [[] for i in range(n + 1)]
+path[1] = [1]
+
+for i in range(2, n + 1):
+    arr[i] = arr[i - 1] + 1
+    path[i] = path[i - 1] + [i]
+    if i % 3 == 0 and arr[i] > arr[i//3] + 1:
+        arr[i] =  arr[i//3] + 1
+        path[i] = path[i//3] + [i]
+    if i % 2  == 0 and arr[i] > arr[i//2] + 1:
+        arr[i] = arr[i//2] + 1
+        path[i] = path[i//2] + [i]
+
+num = n
+print(arr[n])
+for i in path[i][::-1]:
+    print(i, end = ' ')
+# print(n, end = ' ')
+
+# while num != 1:
+#     print(path[num], end = ' ')
+#     num = path[num]
+    
+    
+    
+
 ## 시간초과,, 왤케 같은 실수를 반복할까
 # def dfs(n, cnt, num):
 #     if n == 1:
@@ -28,21 +52,3 @@ path[1] = 0
 # print(arr[0][0])
 # print(' '.join(map(str, arr[0][1])))
 
-for i in range(2, n + 1):
-    arr[i] = arr[i - 1] + 1
-    path[i] = i - 1
-    if i % 3 == 0 and arr[i] > arr[i//3] + 1:
-        arr[i] =  arr[i//3] + 1
-        path[i] = i//3
-    elif i % 2  == 0 and arr[i] > arr[i//2] + 1:
-        arr[i] = arr[i//2] + 1
-        path[i] = i//2
-
-num = n
-
-print(arr[n])
-print(n, end = ' ')
-
-while path[num] != 0:
-    print(path[num], end = ' ')
-    num = path[num]

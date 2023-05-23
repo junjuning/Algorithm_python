1744072c58b5eb414e8a0f394b0bfa9757b5a002
namjiyun <namjiyun3275@gmail.com>
2023-05-23 13:32:07 +0900

    [PRG/큰 수 만들기기]

--- Programmers/Level2/큰수만들기.py
+++ Programmers/Level2/큰수만들기.py
@@ -3,9 +3,13 @@ def solution(number, k):
     totalNum = len(number)-k
 
     for i in range(totalNum): 
-        maxNum = max(number[:len(number)-totalNum+1+i])
+        find = number[:len(number)-totalNum+1+i]
+        if "9" in find:
+            number = number[number.index("9") + 1:]
+            answer.append("9")
+            continue
+        maxNum = max(find)
         number = number[number.index(maxNum)+1:]
         answer.append(maxNum)
-
+        
     return "".join(answer)
-    

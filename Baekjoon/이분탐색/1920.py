dc0c2109a76b33f570d868d3ee7290bcb9ebb529
namjiyun <namjiyun3275@gmail.com>
2023-08-08 13:56:06 +0900

    [BOJ/1920] 수 찾기기

---
+++ Baekjoon/이분탐색/1920.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+nList = list(map(int, sys.stdin.readline().split()))
+m = int(input())
+mList = list(map(int, sys.stdin.readline().split()))
+nList = sorted(nList)
+
+for i in mList:
+    left = 0
+    right = n - 1
+    flag = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if nList[mid] < i:
+            left = mid + 1
+        elif nList[mid] > i:
+            right = mid - 1
+        else:
+            flag = 1
+            break
+    print(flag)

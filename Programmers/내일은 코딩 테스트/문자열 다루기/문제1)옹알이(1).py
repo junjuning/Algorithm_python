99e59fa0aaea376b62b418d66a94489d0812fabc
namjiyun <namjiyun3275@gmail.com>
2023-10-25 17:46:21 +0900

    [PRG/내일은 코딩테스트] 문제1) 옹알이(1)

---
+++ Programmers/내일은 코딩 테스트/문자열 다루기/문제1)옹알이(1).py
@@ -0,0 +1,21 @@
+def solution(babbling):
+    answer = 0
+    arr = ["aya", "ye", "woo", "ma"]
+    
+    for string in babbling:
+        start = 0
+        flag = 0
+        while start < len(string):
+            flag = 0
+            for i in arr:
+                now = string[start:start + len(i)]
+                if i == now:
+                    start = start + len(i)
+                    flag = 1
+                    break
+            if flag == 0:
+                break
+        if flag == 1:
+            answer += 1
+    
+    return answer

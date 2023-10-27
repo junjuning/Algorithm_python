7a01a5cb328f3a5c1b90ade09fc8dab14d5c3b6b
namjiyun <namjiyun3275@gmail.com>
2023-10-27 14:28:22 +0900

    [PRG/내일은 코딩테스트] 문제1) 외계어 사전

--- Programmers/내일은 코딩 테스트/셋집합/문제1)외계어사전.py
+++ Programmers/내일은 코딩 테스트/셋집합/문제1)외계어사전.py
@@ -1,11 +1,22 @@
-from collections import deque
 def solution(spell, dic):
+    answer = 0
+    n = len(spell)
+    spell = set(spell)
     for target in dic:
-        q = deque(spell)
-        for i in target:
-            if i not in q:
-                break
-            q.remove(i)
-        if not q and len(target) == len(spell):
-            return 1
+        if n == len(target):
+            if spell == set(target):
+                return 1
     return 2
+
+
+# from collections import deque
+# def solution(spell, dic):
+#     for target in dic:
+#         q = deque(spell)
+#         for i in target:
+#             if i not in q:
+#                 break
+#             q.remove(i)
+#         if not q and len(target) == len(spell):
+#             return 1
+#     return 2
